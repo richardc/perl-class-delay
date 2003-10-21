1f7d26ac1e32b28e85c1c7d6d73ded80ad4f877c
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-10-21 19:11:34 +0000

    best add some docs then
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Class-Delay@1721 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Class/Delay/Message.pm
+++ lib/Class/Delay/Message.pm
@@ -12,3 +12,42 @@ sub resume {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+Class::Delay::Message - class that represents a delayed message
+
+=head1 METHODS
+
+=head2 package
+
+The package that the delaying method was installed into.
+
+=head2 method
+
+The method being called
+
+=head2 args
+
+The arguments the method was called with.
+
+=head2 is_trigger
+
+True if the Message is a release method.
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2003 Richard Clamp. All Rights Reserved.
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+
+=cut
+
