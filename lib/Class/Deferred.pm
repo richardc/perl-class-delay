a732fa9f6db47b417ce78c3680a9eb235fb0211b
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-10-21 12:04:30 +0000

    another day, another rename
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Class-Delay@1708 c56e198b-7db7-0310-b3ab-8e406f684e33

---
+++ lib/Class/Deferred.pm
@@ -0,0 +1,50 @@
+use strict;
+package Class::Deferred;
+
+our $VERSION = '0.01';
+
+my @deferred;
+sub import {
+    my $package = caller;
+    my $class   = shift;
+    my %args    = @_;
+
+    for my $method (@{ $args{methods} }) {
+        no strict 'refs';
+        *{"$package\::$method"} = sub {
+            push @deferred, {
+                method  => $method,
+                args    => [ @_ ] };
+            return 1;
+        };
+    }
+
+    for my $method (@{ $args{release} }) {
+        my $sub = sub {
+            my $self = shift;
+            # delete our placeholders
+
+
+            # redispatch all the old stuff
+            use Data::Dumper;
+            for my $delayed (@deferred) {
+                warn Dumper $delayed;
+                my $invocant = shift @{ $delayed->{args} };
+                my $method   = $delayed->{method};
+                $invocant->$method( @{ $delayed->{args} } );
+            }
+
+            # splice ourselves out of the isa
+
+            # and redipatch the triggering event
+            return 1;
+        };
+
+        no strict 'refs';
+        *{"$package\::$method"} = $sub;
+    }
+}
+
+
+
+1;
---
+++ t/defer.t
@@ -0,0 +1,33 @@
+#!perl -w
+use strict;
+use Test::More tests => 5;
+
+my $class = 'Class::Deferred';
+my @done;
+my @methods;
+BEGIN { @methods = qw( foo bar baz ) }
+
+for (@methods) {
+    can_ok('Waiter', $_ );
+    ok( Waiter->$_(), "did $_" );
+}
+is( scalar @done, 0, "didn't actually do it yet" );
+
+can_ok( Waiter => 'doit' );
+ok( Waiter->doit, "triggering event" );
+is_deeply( \@done, [ @methods, 'doit' ], "things are now done" );
+
+# testing packages
+package Doer;
+
+for (@methods, 'doit') {
+    no strict 'refs';
+    *{__PACKAGE__."::$_"} = sub { push @done, $_ };
+}
+
+package Waiter;
+our @ISA = 'Doer';
+use Class::Deferred
+  methods => \@methods,
+  release => [qw( doit )];
+
