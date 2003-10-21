6c6d38c217a5f815eee0336cbc03c263a7882ad9
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-10-21 14:49:58 +0000

    start on the pod
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Class-Delay@1716 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Class/Delay.pm
+++ lib/Class/Delay.pm
@@ -45,6 +45,38 @@ sub import {
     }
 }
 
-
-
 1;
+__END__
+
+=head1 NAME
+
+Class::Delay - delay method dispatch until a trigerring event
+
+=head1 SYNOPSIS
+
+ package PrintOut;
+ sub write {
+    my $self = shift;
+    print "printing: ", @_, "\n";
+ }
+ sub flush {
+    print "flushed\n";
+ }
+
+ package DelayedPrint;
+ use base 'PrintOut';
+ use Class::Delay
+    methods => [ 'write' ],
+    release => [ 'flush' ];
+
+ package main;
+
+ DelayedPrint->write( "we'll write this later" ); # won't get through
+                                                  # to PrintOuts 'write' yet
+ DelayedPrint->write( "this too" );
+ DelayedPrint->flush;  # all of the queued call are dispatched
+ DelayedPrint->write( "this won't be delayed" );
+
+=head1 DESCRIPTION
+
+=cut
